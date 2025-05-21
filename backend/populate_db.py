15d3adfe1811dd369b3881113736dc6b26a94961
Rilleb <rickard.elgh@gmail.com>
2025-05-21 20:56:41 +0200

    More courses/quiz on populate script

--- backend/populate_db.py
+++ backend/populate_db.py
@@ -23,25 +23,32 @@ users = [
     create_user("rickard", "rickard@test.com", password),
 ]
 
-num_courses = 3
 
 #  Envariabel course
 # ------------------------------
-envariabel_analys = create_course(
+course = create_course(
     "Envariabel Analys",
     "TATA41",
-    "Kurs som behandlar analyser av en variabel",
+    "Course that covers analysis with one variable",
     users[0],
 )
 
-chapter_1 = create_chapter("Gränsvärden", envariabel_analys, users[0])
+chapter_1 = create_chapter("Limits", course, users[0])
+chapter_2 = create_chapter("Derivates", course, users[0])
 
 quiz_1 = create_quiz(
-    "Grundläggande gränsvärden",
-    envariabel_analys,
+    "Basic limits",
+    course,
     chapter_1,
     users[0],
-    "Detta quiz testar dina grundläggande kunskaper om gränsvärden",
+    "This quiz will test your basic knowledge of limits, free-text and multiple-choice",
+)
+quiz_2 = create_quiz(
+    "Advanced Derivitives",
+    course,
+    chapter_2,
+    users[0],
+    "These are some of the hardest derivates I know, quiz contains only free text answers",
 )
 
 quiz_1_questions = [
@@ -51,22 +58,93 @@ quiz_1_questions = [
     ),
 ]
 
-quiz_1_attempt = create_quiz_attempt(users[0], quiz_1)
-quiz_answers = [
-    create_question_answer(quiz_1_attempt, quiz_1_questions[0], True, False, "1"),
-    create_question_answer(quiz_1_attempt, quiz_1_questions[1], True, True),
+quiz_2_questions = [
+    create_question(quiz_2, "d/dx (2x^2)", 1, False, "4x"),
+    create_question(quiz_2, "d/dx (sin(2x))", 2, False, "2cos(2x)"),
+    create_question(quiz_2, "d/dx (ln(x))", 3, False, "1/x"),
+    create_question(quiz_2, "d/dx (-x^(-1))", 4, False, "x^(-2)"),
 ]
-quiz_1_attempt = change_quiz_attempt(
-    quiz_1_attempt.id, datetime.now() + timedelta(seconds=300), True
-)
+
+# Create 10 quiz attempts for the first quiz on gustav, with him being faster and faster and passing after 5 attempts
+for i in range(10):
+    quiz_1_attempt = create_quiz_attempt(users[0], quiz_1)
+    quiz_answers = [
+        create_question_answer(quiz_1_attempt, quiz_1_questions[0], True, False, "1"),
+        create_question_answer(quiz_1_attempt, quiz_1_questions[1], i > 5, True),
+    ]
+    quiz_1_attempt = change_quiz_attempt(
+        quiz_1_attempt.id, datetime.now() + timedelta(seconds=300 - 10 * i), True
+    )
 # -----------------------------
 
-# More courses...
+# Create 10 quiz attempts for the first quiz on gustav, with him being faster and faster and passing after 5 attempts
+for i in range(10):
+    quiz_1_attempt = create_quiz_attempt(users[0], quiz_1)
+    quiz_answers = [
+        create_question_answer(quiz_1_attempt, quiz_1_questions[0], True, False, "1"),
+        create_question_answer(quiz_1_attempt, quiz_1_questions[1], i > 5, True),
+    ]
+    quiz_1_attempt = change_quiz_attempt(
+        quiz_1_attempt.id, datetime.now() + timedelta(seconds=300 - 10 * i), True
+    )
 
+#  Cybersecurity course
+# ------------------------------
+course = create_course(
+    "Data Security",
+    "TSIT02",
+    "This is a course that gives basic understanding of data security",
+    users[1],
+)
+
+chapter_1 = create_chapter("Web security", course, users[0])
+
+quiz_1 = create_quiz(
+    "Common attacks",
+    course,
+    chapter_1,
+    users[1],
+    "This quiz will test your basic knowledge of web security, multiple-choice questions only. Questions will describe an attack and your job is to identify the category",
+)
+
+quiz_1_questions = [
+    create_question(
+        quiz_1,
+        "Executing/injecting javascript on a victims browser",
+        1,
+        True,
+        "Cross-site scripting/XSS",
+        "SQL-injection",
+        "CSRF",
+        "Directory traversal",
+    ),
+    create_question(
+        quiz_1,
+        "Gaining unauthorized database information via a text input",
+        2,
+        True,
+        "SQL-injection",
+        "Session hijacking",
+        "Credential stuffing",
+        "Broken access control",
+    ),
+    create_question(
+        quiz_1,
+        "Manually trying different passwords to gain access to someones account",
+        3,
+        True,
+        "Brute-force attack",
+        "Dictionary attack",
+        "Credential stuffing",
+        "Cryptographic failure",
+    ),
+]
+
+# -----------------------------
 # Follow courses
-follow_course(users[0], envariabel_analys.id)
-follow_course(users[1], envariabel_analys.id)
-follow_course(users[2], envariabel_analys.id)
+follow_course(users[0], course.id)
+follow_course(users[1], course.id)
+follow_course(users[2], course.id)
 
 # Add friends
 add_friend(users[0], users[1])  # gustav -> thea
